4d2f0466124e75d6419d66eaf003e57503a15622
Caesar0m0d <Caesar0m0d@gmail.com>
2017-03-04 14:53:57 +0900

    467B.c

--- 467B.c
+++ 467B.c
@@ -1,41 +1,60 @@
 #include<stdio.h>
 #include<string.h>
 
-int bi();
+int bi(int m, int k);
 int main(){
 
 int n,m,k,l;
 
 scanf("%d %d %d",&n,&m,&k);
 
-		for(l=0;l<m+1;l++){
-bi();
-				  }
+bi(m,k);
 
 return 0;
 	  }
-int bi(){
-int l=-1,l2;
+int bi(int m, int k){
+int l=-1,l2,cpr=0,chk=0;
 long long xi;
 int b_xi[1050000];
-
+int b_std[1050000];
+long long mm[1002];
+		for(l2=0;l2<m+1;l2++){
 scanf("%lld",&xi);
+mm[l2] = xi;
+				     }
+
 //transform dec to bi
+		for(l2=m;l2>=0;l2--){
+xi=mm[l2];
+
 		while(1){
-b_xi[++l] = xi%2;
+b_xi[l++] = xi%2;
 xi = xi/2;
 
 		if(xi == 0){
-b_xi[++l] = 1;
+b_xi[++l] = 3;
 break;
+			   }	
+		if(l2 == m){
+b_std[l] = b_xi[l];
 			   }
 			}
+//compare friends
+
+		if(l2 != m){
+		for(l2=0;l2<1050000;l2++){
+if(b_std[l2] != b_xi[l2])chk++;
+if(chk > k)break;
+if(b_std[l2] == 3 || b_xi[l2] == 3){
+cpr++;
+break;
+		           	   }	
+					 }
+		     }
+//compare friends
+				    }
 //transform dec to bi
-//test
-		for(l2=0;l2<l;l2++){
-printf("%d\n",b_xi[l2]);
-				 }
-//test
+
 return 0;
 	 }
 
