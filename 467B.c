cae1578d72461e3bb1cc2cc931e7f417a2b09f9d
Caesar0m0d <Caesar0m0d@gmail.com>
2017-03-05 07:03:11 +0900

    467B.c

--- 467B.c
+++ 467B.c
@@ -11,43 +11,57 @@ scanf("%d %d %d",&n,&m,&k);
 
 		for(l=0;l<m+1;l++){
 scanf("%lld",&mm[l]);
-//test
-printf("%lld\n",mm[l]);
-//test
-					 }
 
+					 }
 	bi(m,k,mm);			
 
 return 0;
 	  }
 
 int bi(int m, int k, long long * mm){
-int l,l2,cpr=0,chk=0;
-int t;
+
+int l,l2;
+int temp;
 long long xi;
 int b_xi[1050000];
-int b_std[1050000];
+int b_std[10];
+b_std[0] = 0;
+//b_std = b_xi;
 
 //transform dec to bi
+
 		for(l2=m;l2>=0;l2--){
 xi=mm[l2];
-/*
 l = -1;
 		while(1){
+
 b_xi[++l] = xi%2;
+
 xi = xi/2;
+
 
 		if(xi == 0){
 b_xi[++l] = 3;
+
 break;
 			   }	
-printf("%d\n",l);
 
-		if(l2 == m){
-b_std[l] = b_xi[l];
-			   }
 
-			}*/
+		
+
+			}
+
+//printf("%d\n",l);
+		  if(l2==m){
+		//for(temp=0;temp<l+1;temp++){
+//b_std[0] = 0;
+//b_std[0] = b_xi[0];
+//printf("%d",b_xi[temp]);
+			//}
+//printf("\n");
+			     }
+
+
 /*test
 		for(t=0;t<l;t++){
 printf("%d",b_xi[t]);
@@ -55,8 +69,8 @@ printf("%d",b_xi[t]);
 printf("\n");
 test*/
  
-printf(" xi %lld\n",xi);
-printf("l2 = %d\n",l2);
+//printf(" xi %lld\n",xi);
+//printf("l2 = %d\n",l2);
 				    }
 //l2 lop
 /*test
